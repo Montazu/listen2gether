f226741cfdb7a47f2978ae6b1efcdcbd507b72d9
Montazu <montazu@montazu.pl>
2022-12-18 21:12:45 +0100

    response music info

--- client/pages/api/socket.js
+++ client/pages/api/socket.js
@@ -1,4 +1,5 @@
 import { Server } from 'socket.io'
+import { Innertube } from 'youtubei.js'
 
 export default (req, res) => {
 	if (res.socket.server.io) {
@@ -10,19 +11,31 @@ export default (req, res) => {
 	const io = new Server(res.socket.server)
 	res.socket.server.io = io
 
-	const playlist = [
-		{
-			id: 1,
-			title: 'Góralko',
-			image: 'https://lh3.googleusercontent.com/alkmJH7B8rZnwNJ2x2Ja4wfxta0OXO4-wZA559KKouGJhcSHIQUqlNJfdxgRWxOVHgnQrArwQYCyF0V7Lg=w544-h544-l90-rj',
-			url: 'https://x2convert.com/Thankyou?token=U2FsdGVkX18sTDQIU6aJVCbQYcRYfZIfCdKY%2bE1AZSQwSKNXBX4efnr3a1elY7w6eiPb7PmrZ7e0GAzjypZ1vv%2fi4cH1WSakEwH1bapAUqZSTuxWo8%2bJJLUhK24%2f3UEqanj4QXirFmqdKBWpwXxfByAuDq6u7rXzQpDIMKKy1VY%3d&s=youtube&id=&h=8076912343788754291',
+	const getInfo = async (link) => {
+		const params = new URLSearchParams(new URL(link).search)
+		const id = params.get('v')
+
+		const yt = await Innertube.create()
+		const videoInfo = await yt.actions.execute('/player', {
+			videoId: id,
+			client: 'YTMUSIC_ANDROID', 
+			parse: true 
+		  });
+	  
+		  const title = videoInfo.video_details.title
+		  const { url } = videoInfo.streaming_data.adaptive_formats.pop()
+
+		const info = {
+			id: 3,
+			title,
+			thumbnail: `https://i.ytimg.com/vi/${id}/maxresdefault.jpg`,
+			url
 		}
-	]
+		console.log(info)
+	}
 
 	io.on("connection", (socket) => {
-		// socket.emit('playlist', 'elo')
-
-		socket.on('musicUrl', arg => console.log(arg))
+		socket.on('musicUrl', getInfo)
 	})
 
 	console.log("Setting up socket")
