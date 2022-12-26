7dddf8412a2053d2563178c9c84c3d910b7855e9
Montazu <montazu@montazu.pl>
2022-12-26 20:42:46 +0100

    fixed WebSocket is closed before the connection is established

--- client/src/hooks/useSocket.ts
+++ client/src/hooks/useSocket.ts
@@ -2,15 +2,15 @@ import { useEffect, useRef } from 'react'
 import { io, ManagerOptions, SocketOptions } from 'socket.io-client'
 
 interface useSocketProps {
-	url: string;
-	options: Partial<ManagerOptions & SocketOptions> | undefined;
+	url: string
+	options: Partial<ManagerOptions & SocketOptions> | undefined
 }
 
-export const useSocket = ({ url, options }: useSocketProps ) => {
+export const useSocket = ({ url, options }: useSocketProps) => {
 	const { current: socket } = useRef(io(url, options))
 
 	useEffect(() => {
-		if (socket) socket.close()
+		if (socket && document.readyState === 'complete') socket.close()
 	}, [socket])
 
 	return socket
